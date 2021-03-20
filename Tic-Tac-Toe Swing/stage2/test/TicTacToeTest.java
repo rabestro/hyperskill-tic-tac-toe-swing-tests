53da4f6c18ba7caaf7c471439b42c8456f54b9d2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-21 00:12:03 +0200

    Added more games for last test

--- Tic-Tac-Toe Swing/stage2/test/TicTacToeTest.java
+++ Tic-Tac-Toe Swing/stage2/test/TicTacToeTest.java
@@ -10,7 +10,10 @@ import tictactoe.TicTacToe;
 import javax.swing.JButton;
 import java.text.MessageFormat;
 import java.text.StringCharacterIterator;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static java.util.stream.IntStream.range;
@@ -170,7 +173,28 @@ public class TicTacToeTest extends SwingTest {
     private String[][] testGame = new String[][]{
             {"A1", "______X__", "P"}, {"B1", "______XO_", "P"},
             {"C3", "__X___XO_", "P"}, {"B3", "_OX___XO_", "P"},
-            {"B2", "_OX_X_XO_", "X"}, {"RS", "_________", "E"}
+            {"B2", "_OX_X_XO_", "X"}, {"RS", "_________", "E"},
+
+            {"B2", "____X____", "P"}, {"A1", "____X_O__", "P"},
+            {"C1", "____X_O_X", "P"}, {"A3", "O___X_O_X", "P"},
+            {"A2", "O__XX_O_X", "P"}, {"C2", "O__XXOO_X", "P"},
+            {"B3", "OX_XXOO_X", "P"}, {"B1", "OX_XXOOOX", "P"},
+            {"C3", "OXXXXOOOX", "D"}, {"B2", "OXXXXOOOX", "D"},
+            {"B2", "OXXXXOOOX", "D"}, {"RS", "_________", "E"},
+
+            {"A2", "___X_____", "P"}, {"B2", "___XO____", "P"},
+            {"A1", "___XO_X__", "P"}, {"A3", "O__XO_X__", "P"},
+            {"C1", "O__XO_X_X", "P"}, {"B1", "O__XO_XOX", "P"},
+            {"C2", "O__XOXXOX", "P"}, {"B3", "OO_XOXXOX", "O"},
+            {"A3", "OO_XOXXOX", "O"}, {"C3", "OO_XOXXOX", "O"},
+            {"C3", "OO_XOXXOX", "O"}, {"B2", "OO_XOXXOX", "O"},
+            {"RS", "_________", "E"}, {"RS", "_________", "E"},
+
+            {"C1", "________X", "P"}, {"B1", "_______OX", "P"},
+            {"B2", "____X__OX", "P"}, {"C2", "____XO_OX", "P"},
+            {"A3", "X___XO_OX", "X"}, {"B3", "X___XO_OX", "X"},
+            {"C3", "X___XO_OX", "X"}, {"A1", "X___XO_OX", "X"},
+            {"A1", "X___XO_OX", "X"}, {"RS", "_________", "E"},
     };
 
     @DynamicTest(data = "testGame", feedback = "Incorrect state of the game")
